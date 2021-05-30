a6b8a053a49c06f904e61be7d74ac8e7f8b2ab91
tgmeng <tgmeng@163.com>
2021-05-30 10:57:16 +0800

    refactor: prevMethod 优化

--- src/switch_input_method_mac.mm
+++ src/switch_input_method_mac.mm
@@ -1,25 +1,26 @@
 #include <Carbon/Carbon.h>
 #include "./switch_input_method.h"
 
-static NSString *prevMethod = NULL;
+static NSMutableString* prevMethod = [[NSMutableString alloc] init];
 
 bool switchEnglishInputMethod () {
     TISInputSourceRef currentSource = TISCopyCurrentKeyboardInputSource();
-    prevMethod = (__bridge NSString *)(TISGetInputSourceProperty(currentSource, kTISPropertyInputSourceID));
+    [prevMethod setString:(__bridge NSString *)(TISGetInputSourceProperty(currentSource, kTISPropertyInputSourceID))];
 
     NSArray* sources = CFBridgingRelease(TISCreateInputSourceList((__bridge CFDictionaryRef)@{ (__bridge NSString*)kTISPropertyInputSourceID : @"com.apple.keylayout.ABC" }, false));
     TISInputSourceRef source = (__bridge TISInputSourceRef)sources[0];
-    OSStatus status = TISSelectInputSource(source);
 
-    return status != noErr;
+    OSStatus status = TISSelectInputSource(source);
+    return status == noErr;
 }
 
 bool restorePreviousInputMethods() {
-    if (prevMethod == NULL) {
+    if ([prevMethod length] == 0) {
         return false;
     }
     NSArray* sources = CFBridgingRelease(TISCreateInputSourceList((__bridge CFDictionaryRef)@{ (__bridge NSString*)kTISPropertyInputSourceID : prevMethod }, false));
     TISInputSourceRef source = (__bridge TISInputSourceRef)sources[0];
+
     OSStatus status = TISSelectInputSource(source);
-    return status != noErr;
+    return status == noErr;
 }
